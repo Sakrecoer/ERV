513b2894102fd162cf53bf3d40fc516162d82e52
Set Hallstrom <set@hallstrom.ch>
2019-04-15 12:18:31 +0200

    slicing up the evgen pack

--- assets/js/kalk_window.js
+++ assets/js/kalk_window.js
@@ -1,12 +1,5 @@
 /*
-Bones Scripts File
-Author: Eddie Machado
-
-This file should contain any js scripts you want to add to the site.
-Instead of calling it in the header or throwing it inside wp_head()
-this file will be called automatically in the footer so as not to
-slow the page load.
-
+Written by Evgenij Bondaryev
 */
 jQuery(document).ready(function($) {
 
@@ -76,7 +69,7 @@ jQuery(document).ready(function($) {
 
 });
 
-// Priser fönsterputs
+// Priser fonsterputs
 
 var room_price = {
 
