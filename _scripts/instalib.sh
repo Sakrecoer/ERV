9a037609d5d9124f214d1bfdaa22c46c329ee0a8
Set Hallstrom <set@hallstrom.ch>
2019-05-09 10:51:42 +0200

    remove deleted images too

--- _scripts/instalib.sh
+++ _scripts/instalib.sh
@@ -4,6 +4,7 @@ cd /home/set/git/instalib/
 
 npm run instalib -- liberate https://www.instagram.com/enrenarevardag/ -o instagram.yml
 rm -f media/*.jpg
+rm -f /home/set/git/erv.com/images/insta/*
 
 npm run instalib mirror instagram.yml 
 mogrify -resize 220 /home/set/git/instalib/media/*.jpg
