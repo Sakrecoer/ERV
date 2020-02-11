3a8e57f23814c5045b100a81db4da715cb83379b
Set Hallstrom <set@hallstrom.ch>
2020-02-11 13:17:17 +0100

    remove redundant include and move analytics to production

--- _includes/analytics.js
+++ _includes/analytics.js
@@ -1,10 +1,11 @@
 {% if page.url == '/404/' %}
 
-<!-- Matomo -->
+//* Matomo *//
 
   var _paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['setDocumentTitle',  '404/URL = ' +  encodeURIComponent(document.location.pathname+document.location.search) + '/From = ' + encodeURIComponent(document.referrer)]);
+  _paq.push(['disableCookies']);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
@@ -15,17 +16,17 @@
     g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.matomo.cloud/terminalnetwork.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
   })();
 
-<!-- End Matomo Code -->
+//* End Matomo *//
 
 
 
 {% else %}
-<!-- Matomo Tag Manager -->
+//* Matomo Tag Manager *//
 
   var _mtm = _mtm || [];
   _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src='https://cdn.matomo.cloud/terminalnetwork.matomo.cloud/container_BAJ4B6LU_dev_7b70f293d0372da7890d025a.js'; s.parentNode.insertBefore(g,s);
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src='https://cdn.matomo.cloud/terminalnetwork.matomo.cloud/container_BAJ4B6LU.js'; s.parentNode.insertBefore(g,s);
 
-<!-- End Matomo Tag Manager -->
+//* End Matomo Tag Manager *//
 {% endif %}
